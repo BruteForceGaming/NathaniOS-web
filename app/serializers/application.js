71a53e15e83cd6bd136abb8c27dbd2a67b77a18d
Damon Blais <damon.blais@gmail.com>
2019-09-21 21:35:17 -0700

    remove unused commented function

--- app/serializers/application.js
+++ app/serializers/application.js
@@ -1,10 +1,5 @@
 import DS from 'ember-data';
 
-// const capitalize = (s) => {
-//   if (typeof s !== 'string') return ''
-//   return s.charAt(0).toUpperCase() + s.slice(1)
-// }
-
 export default DS.JSONAPISerializer.extend({
   keyForAttribute: function(key) {
     return key;
