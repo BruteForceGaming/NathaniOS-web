b5b604cc5f5018c39815f5b4f47118d5c6cacfbe
Damon Blais <damon.blais@gmail.com>
2019-09-23 00:38:09 -0700

    model() -> async model()

--- app/routes/ladders.js
+++ app/routes/ladders.js
@@ -1,7 +1,7 @@
 import Route from '@ember/routing/route';
 
-export default Route.extend({
-  model() {
+export default class extends Route {
+  async model() {
     return this.store.loadRecords('ladder');
   }
-});
+};
