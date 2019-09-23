056fbc2e301c4cd5c2d879f74a0d977e4255b0d8
Damon Blais <damon.blais@gmail.com>
2019-09-22 17:41:04 -0700

    fix: requests sent with a double slash before path

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -2,5 +2,5 @@ import JSONAPIAdapter from 'ember-data/adapters/json-api';
 
 export default JSONAPIAdapter.extend({
     // host: "http://localhost:3000"
-  host: 'http://134.209.74.55:3000/'
+  host: 'http://134.209.74.55:3000'
 });
